c5a3f73616c5e2bda2d16a9887a9f872c97b9c3b
Hbotello436 <utp0159351@alumno.utpuebla.edu.mx>
2025-04-04 16:11:04 -0600

    Update Datos_Demograficos.route.js

--- routes/Datos_Demograficos.route.js
+++ routes/Datos_Demograficos.route.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const {
-    getReviews,
-    getReviewById,
-    setReview,
-    updateReview,
-    deleteReviewById
-} = require('../controllers/review.controller');
+    getDatos_Demografico,
+    getDatos_DemograficosById,
+    setDatos_Demograficos,
+    updateDatos_Demograficos,
+    deleteDatos_DemograficosById,
+} = require('../controllers/Datos_Demograficos.controller');
 
-// Endpoint para recuperar toda la colección de Reviews
-router.get('/all', getReviews);
-// Endpoint para recuperar un elemento de la colección Review
-router.get('/ById/:id', getReviewById);
-// Endpoint para agregar un nuevo elemento a la coleeción Reviews
-router.post('/add', setReview);
-// Endpoint para la actualización de un elemento de la colección Reviews
-router.put('/modify/:id', updateReview);
-// Endpoint para la eliminación de un elemento de la colección Review
-router.delete('/less/:id', deleteReviewById);
+// Endpoint para recuperar toda la colección de Datos_Demograficos
+router.get('/all', getDatos_Demografico);
+// Endpoint para recuperar un elemento de la colección Datos_Demograficos
+router.get('/ById/:id', getDatos_DemograficosById);
+// Endpoint para agregar un nuevo elemento a la coleeción Datos_Demografico
+router.post('/add', setDatos_Demograficos);
+// Endpoint para la actualización de un elemento de la colección Datos_Demografico
+router.put('/modify/:id', updateDatos_Demograficos);
+// Endpoint para la eliminación de un elemento de la colección Datos_Demograficos
+router.delete('/less/:id', deleteDatos_DemograficosById);
 
 module.exports = router;
+
+
 
 
